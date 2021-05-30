f0740cf3433b851105de364c4d6b45f3a013f37c
frpericacho <frpericacho@gmail.com>
2021-05-30 19:36:08 +0200

    implementado lectura/escritura con la BD

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,19 +1,46 @@
 import { Text, View } from "react-native";
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from "react-native-elements";
 import { supabase } from "../lib/SupabaseSetUp"
 
+type Chat = {
+  id: number
+  title: string
+  description: string
+  user_id: number
+  avatar_url: string
+}
+
 export default function Home({navigation}) {
+  const [chats, setChats] = useState<Array<Chat>>([])
+  
+  const fetchChat = async () => {
+    const { data: chats, error } = await supabase
+      .from<Chat>('chats')
+      .select('*')
+      .order('id', { ascending: false })
+    if (error) console.log('error', error)
+    else setChats(chats!)
+  }
+
   return(
-      <View>
-          <Button 
-            title="cerrar sesion"
-            onPress={()=>supabase.auth.signOut()}
-          />
-          <Button 
-            title="A la segunda vista"
-            onPress={()=>navigation.navigate('SecondHome')}
-          />
-      </View>
+    <View>
+      <Button 
+        title="cerrar sesion"
+        onPress={()=>supabase.auth.signOut()}
+      />
+      <Button 
+        title="prueba"
+        onPress={()=>fetchChat()}
+      />
+      <Button 
+        title="console"
+        onPress={()=>console.log('los chats',chats)}
+      />
+      <Button 
+        title="A la segunda vista"
+        onPress={()=>navigation.navigate('SecondHome')}
+      />
+    </View>
   );
 };
