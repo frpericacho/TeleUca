e601139456a3c0f264c4cca55837c38be1b858ec
frpericacho <frpericacho@gmail.com>
2021-08-10 18:51:12 +0200

    implementado notificaciones push-up; trabjando en badge de mensajes nuevos

--- components/chatItem.tsx
+++ components/chatItem.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { View, StyleSheet } from 'react-native';
 import {Avatar,Title} from 'react-native-paper'
+import {Badge, withBadge} from 'react-native-elements'
 import firebase from "firebase";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ChatItem = ({Chat, navigation}:any) => {
     //MyUser
@@ -11,15 +13,22 @@ const ChatItem = ({Chat, navigation}:any) => {
     if(Chat.group){
         return(
             <TouchableOpacity onPress={()=>{navigation.navigate('Chat',Chat)}}>
-                <View style={{flexDirection:'row', backgroundColor: '#00bde6', height:75, alignItems:'center', marginBottom:1}}>
-                    <Avatar.Image
-                        source={Chat.avatar_url ? {uri:Chat.avatar_url} : {uri:'../assets/icon.png'}}
-                        size={50}
-                        style={{marginLeft:20}}
-                    />
-                    <View style={{marginLeft:15, flexDirection:'column'}}>
-                        <Title style={styles.title}>{Chat.title}</Title>
-                        <Title style={styles.subTitle}>{Chat.description}</Title>
+                <View style={{flexDirection:'row', backgroundColor: '#00bde6', height:75, width:'100%', alignItems:'center', marginBottom:1}}>
+                    <View style={{flexDirection:'column'}}>
+                        <Avatar.Image
+                            source={Chat.avatar_url ? {uri:Chat.avatar_url} : {uri:'../assets/icon.png'}}
+                            size={50}
+                            style={{marginLeft:20}}
+                        />
+                        <Badge
+                            status="success"
+                            containerStyle={{ position: 'absolute', top: -4, right: -4 }}
+                            value="!"
+                        />
+                    </View>
+                    <View style={{marginLeft:15, flexDirection:'column', width:'60%'}}>
+                        <Title adjustsFontSizeToFit style={styles.title}>{Chat.title}</Title>
+                        <Title adjustsFontSizeToFit style={styles.subTitle}>{Chat.description}</Title>
                     </View>
                 </View>
             </TouchableOpacity>
@@ -32,14 +41,21 @@ const ChatItem = ({Chat, navigation}:any) => {
         
         return(
             <TouchableOpacity onPress={()=>{navigation.navigate('Chat',Chat)}}>
-                <View style={{flexDirection:'row', backgroundColor: '#00bde6', height:75, alignItems:'center', marginBottom:1}}>
-                    <Avatar.Image
-                        source={Chat.avatar_url ? {uri:Chat.avatar_url} : {uri:'../assets/icon.png'}}
-                        size={50}
-                        style={{marginLeft:20}}
-                    />
-                    <View style={{marginLeft:15, flexDirection:'column'}}>
-                        <Title style={styles.title}>{titleDisplay[0]}</Title>
+                <View style={{flexDirection:'row', backgroundColor: '#00bde6', height:75, width:'100%', alignItems:'center', marginBottom:1}}>
+                    <View style={{flexDirection:'column'}}>
+                        <Avatar.Image
+                            source={Chat.avatar_url ? {uri:Chat.avatar_url} : {uri:'../assets/icon.png'}}
+                            size={50}
+                            style={{marginLeft:20}}
+                        />
+                        <Badge
+                            status="success"
+                            containerStyle={{ position: 'absolute', top: -4, right: -4 }}
+                            value="!"
+                        />
+                    </View>
+                    <View style={{marginLeft:15, flexDirection:'column', width:'60%'}}>
+                        <Title adjustsFontSizeToFit style={styles.title}>{titleDisplay[0].split('@')[0]}</Title>
                     </View>
                 </View>
             </TouchableOpacity>
