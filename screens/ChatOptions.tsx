e05b7a8a7e7483ac6e32d4c8043b77757930566d
frpericacho <frpericacho@gmail.com>
2021-08-18 17:49:03 +0200

    trabajando en admin elimine a un usuario de un chat

--- screens/ChatOptions.tsx
+++ screens/ChatOptions.tsx
@@ -3,6 +3,7 @@ import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Image } from "react-native-elements";
 import UserItem from '../components/userItem';
+import { SwipeListView } from 'react-native-swipe-list-view';
 import firebase from "firebase";
 
 const ChatOptions = ({route,navigation}:any) => {
@@ -33,6 +34,54 @@ const ChatOptions = ({route,navigation}:any) => {
     },[])
     
     if(route.params.Admin){
+
+        //SWIPER
+        //https://www.youtube.com/watch?v=k-Ra0tdCEOc
+
+        return(
+            <View style={{flexDirection:'column', height:'100%', justifyContent:'flex-start'}}>
+                <View style={{backgroundColor:'white', height:'7%', flexDirection:'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                    <View style={{ marginLeft:15}}>
+                        <TouchableOpacity>
+                            <Icon name="arrow-left" size={30} color="#900" onPress={()=>navigation.goBack()}/>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <Text>
+                            {route.params.title}
+                        </Text>
+                    </View>
+                    <View style={{marginRight:15}}>
+                        <Icon name="dots-vertical" size={30} color="white"/>
+                    </View> 
+                </View>
+                <View style={{height:'93%', flex:1, flexDirection: "column"}}>
+                    <View style={{flex:1}}>
+                        <Image
+                            source={{uri:route.params.chat.avatar_url}}
+                            style={{ height: '100%', resizeMode: 'contain' }}
+                        />
+                    </View>
+                    <View style={{ flex: 2, backgroundColor: "darkorange" }} >
+                        <Text>
+                            Participantes
+                        </Text>
+                        <SwipeListView
+                            data={users}
+                            renderItem={renderUserItem}
+                            renderHiddenItem={ (data, rowMap) => (
+                                <View>
+                                    <Text>Left</Text>
+                                    <Text>Right</Text>
+                                </View>
+                            )}
+                            rightOpenValue={-75}
+                        />
+                    </View>
+                </View>
+            </View>
+        )
+    }else{
         return(
             <View style={{flexDirection:'column', height:'100%', justifyContent:'flex-start'}}>
                 <View style={{backgroundColor:'white', height:'7%', flexDirection:'row', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -67,37 +116,6 @@ const ChatOptions = ({route,navigation}:any) => {
                             renderItem={renderUserItem}
                             keyExtractor={(item) => item.email}
                         />
-                    </View>
-                </View>
-            </View>
-        )
-    }else{
-        return(
-            <View style={{flexDirection:'column', height:'100%', justifyContent:'flex-start'}}>
-                <View style={{backgroundColor:'white', height:'7%', flexDirection:'row', justifyContent: 'space-between', alignItems: 'center'}}>
-                    <View style={{ marginLeft:15}}>
-                        <TouchableOpacity>
-                            <Icon name="arrow-left" size={30} color="#900" onPress={()=>navigation.goBack()}/>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <Text>
-                            {route.params.title}
-                        </Text>
-                    </View>
-                    <View style={{marginRight:15}}>
-                        <Icon name="dots-vertical" size={30} color="white"/>
-                    </View> 
-                </View>
-                <View style={{height:'93%', flex:1, flexDirection: "column"}}>
-                    <View style={{flex:1}}>
-                        <Image
-                            source={{uri:route.params.chat.avatar_url}}
-                            style={{ height: '100%', resizeMode: 'contain' }}
-                        />
-                    </View>
-                    <View style={{ flex: 2, backgroundColor: "darkorange" }} >
-                        
                     </View>
                 </View>
             </View>
